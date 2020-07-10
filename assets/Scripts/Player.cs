c0fbcc9fd55a25a8739c427758e805eab41b287a
Cvaniak <igna.cwaniak@gmail.com>
2020-07-11 00:17:07 +0200

    Just commit

---
+++ assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject test;
+    
+    void Start()
+    {
+        Rigid
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
