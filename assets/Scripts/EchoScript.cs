7ea8333a7fa1ec154d3c251fb4a5e3b139c6c904
Cvaniak <igna.cwaniak@gmail.com>
2020-07-11 18:14:26 +0200

    A lot

---
+++ assets/Scripts/EchoScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EchoScript : MonoBehaviour
+{
+    private float timeBtwSpawns;
+    public float startTimeBtwSpawns;
+
+    public GameObject echo;
+    public Rigidbody2D playerRigidbody;
+    public float velocityTresh;
+  
+    void Start()
+    {
+        
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerRigidbody.velocity.sqrMagnitude > velocityTresh*velocityTresh){
+
+        if(timeBtwSpawns <= 0 ){
+            GameObject g = Instantiate(echo, playerRigidbody.transform.position,  playerRigidbody.transform.rotation);
+            Destroy(g, 1);
+            timeBtwSpawns = startTimeBtwSpawns;
+        }
+        else{
+            timeBtwSpawns -= Time.deltaTime;
+        }
+        }
+    }
+}
