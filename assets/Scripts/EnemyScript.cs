e59bc8f9104ea9abfc57decbe6bfb9fe43daf29d
jaolejnik <olejnik.jakub71@gmail.com>
2020-07-11 13:56:21 +0200

    Improved pathfinding via script

--- assets/Scripts/EnemyScript.cs
+++ assets/Scripts/EnemyScript.cs
@@ -1,23 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class EnemyScript : Pistol
 {
 
-    private Vector2 playerPos;
     public GameObject playerPrefab;
+    private Vector2 playerPos;
     private Rigidbody2D playerRigidbody;
+
+    // Pathfinding vars
+    public Transform target;
+    public float speed = 200f;
+    public float nextWaypointDistance = 3f;
+    Path path;
+    int currentWaypoint;
+    bool reachedEndOfPath = false;
+    Seeker seeker;
+    Rigidbody2D rb;
+
+    void OnPathComplete(Path p)
+    {
+      if (!p.error)
+      {
+        path = p;
+        currentWaypoint = 0;
+      }
+    }
 
     void Start()
     {
         SpawnEffect();
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+        seeker.StartPath(rb.position, target.position, OnPathComplete);
         playerRigidbody = playerPrefab.GetComponent<Rigidbody2D>();
     }
+
     void Update()
     {
         PlayerPosition();
     }
+
     void FixedUpdate() {
         LookAtPlayer();
         isAlive();
@@ -31,4 +56,6 @@ public class EnemyScript : Pistol
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         pistolBody.rotation = angle;
     }
+
+
 }
