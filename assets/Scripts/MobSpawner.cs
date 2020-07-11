62600d44b7a3550f7af69fb4b33300809ee14b73
Cvaniak <igna.cwaniak@gmail.com>
2020-07-11 14:03:07 +0200

    Prefabs add

---
+++ assets/Scripts/MobSpawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // GameObject ga;
+    float spawnNext = 2;
+    public GameObject _enemy;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(spawnNext == 2){
+            StartCoroutine(SpawnWave());
+            Debug.Log("StartCoutine");
+        }
+        if(!EnemyIsAlive() && spawnNext == 0){
+            spawnNext = 2;
+            Debug.Log("EnemyIsAlive");
+        }
+
+    }
+
+    IEnumerator SpawnWave(){
+
+        spawnNext = 1;
+
+        yield return new WaitForSeconds(5);
+        Instantiate(_enemy, gameObject.transform.position, Quaternion.identity);
+        spawnNext = 0;
+        yield break;
+    }
+
+    bool EnemyIsAlive(){
+        // Debug.Log(GameObject.FindGameObjectsWithTag("Enemy").Length);
+        if(GameObject.FindGameObjectWithTag("Enemys") == null){
+            return false;
+        }
+        return true;
+    }
+    public void SpwanNextSwitch(){
+        spawnNext = 2;
+
+    }
+}
---
+++ assets/Scripts/PistolSpawnScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PistolSpawnScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void FixedUpdate() {
+        gameObject.transform.rotation = Quaternion.identity;
+    }
+}
