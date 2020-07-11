700d53cc91306503d6ddf8bf47a694a7af969e9d
Cvaniak <igna.cwaniak@gmail.com>
2020-07-11 11:50:43 +0200

    Bullet Effect

--- assets/Scripts/BulletScript.cs
+++ assets/Scripts/BulletScript.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class BulletScript : MonoBehaviour
 {
+    
+    public Light2D light2D;
+    public GameObject hitEffect;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,22 @@ public class BulletScript : MonoBehaviour
     void Update()
     {
         
+    }
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        
+        Debug.Log(hitInfo.name);
+        if (hitInfo.CompareTag("Enemy")){
+            EnemyScript enemy = hitInfo.GetComponent<EnemyScript>();
+            if(enemy != null){}
+                // enemy.Die();
+            else
+                Debug.Log("Lel");
+        }
+        if(!hitInfo.CompareTag("Player")){
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 4f);
+        Destroy(gameObject);
+        }
     }
 }
