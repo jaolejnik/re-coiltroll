c201413bb44d8e0a49a53ccd7bcf1cc9b6a77844
jaolejnik <olejnik.jakub71@gmail.com>
2020-07-12 18:28:47 +0200

    Updated main scene

---
+++ assets/Scripts/CrosshairScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairScript : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject crossHair;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      crossHair.transform.position = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
---
+++ assets/Scripts/StartScreenScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreenScript : MonoBehaviour
+{
+    // public startBlock;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D hitInfo)
+    {
+      Debug.Log(hitInfo.gameObject.name);
+      // if(hitInfo.gameObject.name == "StartBlock")
+      // {
+      //   Debug.Log("ŚCIANA UDERZONA");
+      //   Invoke("ChamgeScene", 1f);
+      // }
+    }
+
+    void ChangeScene()
+    {
+      SceneManager.LoadScene("main", LoadSceneMode.Additive);
+    }
+}
