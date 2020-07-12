65ff37bd78e96a4510288c82e17e553621c2332b
jaolejnik <olejnik.jakub71@gmail.com>
2020-07-12 03:43:55 +0200

    Added knife enemy

--- assets/Scripts/BulletScript.cs
+++ assets/Scripts/BulletScript.cs
@@ -5,23 +5,23 @@ using UnityEngine.Experimental.Rendering.Universal;
 
 public class BulletScript : MonoBehaviour
 {
-    
+
     public Light2D light2D;
     public GameObject hitEffect;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     void OnTriggerEnter2D(Collider2D hitInfo)
     {
-        
+
         Debug.Log(hitInfo.name);
         if (hitInfo.CompareTag("Enemys")){
             EnemyScript enemy = hitInfo.GetComponent<EnemyScript>();
@@ -30,6 +30,13 @@ public class BulletScript : MonoBehaviour
             }
             else{
                 Debug.Log("Lel");
+            }
+            Knife knife = hitInfo.GetComponent<Knife>();
+            if(knife != null){
+                knife.Hit(1);
+            }
+            else{
+                Debug.Log("Lel knife");
             }
         }
         if(!hitInfo.CompareTag("Players")){
---
+++ assets/Scripts/Knife.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class Knife : MonoBehaviour
+{
+    public Rigidbody2D knifeRigidBody;
+    public Rigidbody2D playerRigidbody;
+    public float dashForce = 10f;
+    public float minDistance = 10f;
+    public float hp = 1;
+    GameObject playerPrefab;
+    Transform playerTransform;
+    Transform knifeTransform;
+    bool charging = false;
+
+    public AudioClip aimSound;
+    public AudioClip chargeSound;
+    public AudioClip collisionSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      playerPrefab = GameObject.FindWithTag("Player");
+      playerRigidbody = playerPrefab.GetComponent<Rigidbody2D>();
+      playerTransform = playerRigidbody.transform;
+      knifeTransform = knifeRigidBody.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      Charge();
+    }
+
+    void FixedUpdate()
+    {
+      if (!charging)
+        LookAtPlayer();
+      isAlive();
+    }
+
+    void Charge()
+    {
+      RaycastHit2D hitInfo = Physics2D.Raycast(knifeTransform.position, Vector2.up);
+
+      if (hitInfo)
+      {
+        float distance = Vector2.Distance(knifeTransform.position, playerTransform.position);
+        if (distance <= minDistance)
+        {
+          charging = true;
+
+          Invoke("Dash", 1.5f);
+        }
+      }
+    }
+
+    void Dash()
+    {
+      // AudioSource.PlayClipAtPoint(aimSound, knifeRigidBody.transform.position, 1f);
+      knifeRigidBody.AddForce(0.1f*dashForce*knifeTransform.up, ForceMode2D.Impulse);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+      Destroy(gameObject);
+    }
+
+    void LookAtPlayer()
+    {
+      Vector2 lookDir = playerTransform.position - knifeTransform.position;
+      float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+      knifeRigidBody.rotation = angle;
+    }
+
+    void isAlive(){
+        if(hp <= 0){
+            Die();
+        }
+    }
+    public void Hit(float damage){
+        hp -= damage;
+    }
+    void Die(){
+        Destroy(gameObject);
+    }
+}
